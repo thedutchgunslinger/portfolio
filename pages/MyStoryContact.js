57d71d392fabac7d12742a2576c8b203322ef844
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2022-01-23 16:33:33 +0100

    contact linkjes toegevoegd

--- pages/MyStoryContact.js
+++ pages/MyStoryContact.js
@@ -9,9 +9,15 @@ const MyStoryContact = () => {
     <div className={browserLayoutStyles.flexContact}>
       <h2>Contact</h2>
       <div className={browserLayoutStyles.flexIcons}>
-        <Image src={mailIcon} alt="Mail icon" width={45} height={30} />
-        <Image src={instaIcon} alt="Instagram icon" width={35} height={35} />
-        <Image src={linkedinIcon} alt="Linkedin icon" width={35} height={35} />
+        <a href="mailto:noahbeij@gmail.com?subject=Stage">
+          <Image src={mailIcon} alt="Mail icon" width={45} height={30} />
+        </a>
+        <a href="https://www.instagram.com/dev.nbeij/" target="blank">
+          <Image src={instaIcon} alt="Instagram icon" width={35} height={35} />
+        </a>
+        <a href="https://www.linkedin.com/in/noah-beij-6a56a5177/" target="blank">
+          <Image src={linkedinIcon} alt="Linkedin icon" width={35} height={35} />
+        </a>
       </div>
     </div>
   );
