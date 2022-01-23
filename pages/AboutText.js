a5f9346161ca4a0a5dcdeed30bf7a72c6e433081
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2022-01-23 16:59:49 +0100

    update tekst styles

--- pages/AboutText.js
+++ pages/AboutText.js
@@ -8,7 +8,7 @@ const AboutText = () => {
         dan dat. Ik ben een webdeveloper, leraar, content creator, een vriend,
         zoon, broer, een student, een muziek maker, een ondernemer. Ik werk met
         een passie om mensen te helpen. Dat kan een nieuwe site zijn, een design
-        of het doorgeven van kennis Met meer dan 6 jaar ervaring hoop ik te
+        of het doorgeven van kennis. <br/> Met meer dan 6 jaar ervaring hoop ik te
         kunnen zeggen dat ik al veel gedaan te hebben. Zo zijn de talen HTML,
         CSS, JavaScript, PHP en MySQL mij niet onbekend. Ik ben begonnen op de
         Havo met informatica. Daar werd mijn interesse gewekt dat ik zelf vanuit
@@ -17,7 +17,7 @@ const AboutText = () => {
         aansprak. Dit heb ik gevonden bij Fontys Tilburg. Doordat hier op
         projectbasis gewerkt wordt heb ik mij kunnen ontwikkelen in diverse
         competenties. Na schooltijd werk ik af en toe voor Fontys videocrew. Ik
-        stel hier de projecten voor op (scripts) en presteer regelmatig de
+        stel hier de projecten voor op (scripts) en presenteer regelmatig de
         video's. Wat ik erg leuk vind. Zo heb ik aandacht voor details,
         transferabel code zal netjes zijn en voor andere te lezen. Mocht er iets
         niet gaan zoals ik het wil kan ik door mijn aanpassingsvermogen en
@@ -28,13 +28,13 @@ const AboutText = () => {
         team op zijn best te kunnen laten functioneren. Analyseren wat een klant
         graag wil en of dit haalbaar is, is het eerste waar ik mee begin.
         Tussentijds en achteraf het presenteren van de ideeën en het werk dat je
-        gedaan. Wat ik in de lockdown de periode heb geleerd is discipline.
+        gedaan. Wat ik in de lockdown de periode heb geleerd is discipline. <br/>
         Tijdens de eerste 2 semesters hebben veel collega studenten hun weg naar
         mij gevonden voor uitleg van de leerstof. Zij vonden de manier waarop
         erg prettig maar didactische vaardigheden hebben we op school nog niet
         gehad en ik hoop deze bij jullie te kunnen leren, om mijn specialisatie
         ICT en educatie te kunnen afronden. Ik heb gemerkt dat ik kennis
-        overdragen erg leuk vindt en mij energie geeft. In de toekomst zou ik
+        overdragen erg leuk vindt en mij energie geeft. <br/> In de toekomst zou ik
         graag als leraar op school willen werken, afgewisseld met een aantal
         uren in mijn eigen web development bedrijf.
       </p>
