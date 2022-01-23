8a88b95d35a61cbe44897067e2f97ee3ff2e8749
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2022-01-23 16:55:51 +0100

    about tekst

--- pages/AboutText.js
+++ pages/AboutText.js
@@ -2,17 +2,41 @@ const AboutText = () => {
   return (
     <div>
       <p>
-        Im not just a designer, im more then that. I am a developer, an
-        educator, a content creator, a friend, a student, a musician. I work
-        from a passion to help people, that could be a site, a design or
-        teaching a subject. with more then 6 years experience moet ik echt ff
-        snel een fatsoenlijke tekst gaan schrijven om hier neer te zetten met
-        een goeie lengte Lorem ipsum dolor sit amet, consetetur sadipscing
-        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-        est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        Hoi, ik ben Noah Beij, 21 jaar, woon in Tilburg maar kom oorspronkelijk
+        uit Limburg. De Limburgse taal spreek ik niet, de taal die mij beter
+        past is web development. Ik ben niet zomaar een webdesigner, ik ben meer
+        dan dat. Ik ben een webdeveloper, leraar, content creator, een vriend,
+        zoon, broer, een student, een muziek maker, een ondernemer. Ik werk met
+        een passie om mensen te helpen. Dat kan een nieuwe site zijn, een design
+        of het doorgeven van kennis Met meer dan 6 jaar ervaring hoop ik te
+        kunnen zeggen dat ik al veel gedaan te hebben. Zo zijn de talen HTML,
+        CSS, JavaScript, PHP en MySQL mij niet onbekend. Ik ben begonnen op de
+        Havo met informatica. Daar werd mijn interesse gewekt dat ik zelf vanuit
+        thuis al veel cursussen heb gevolgd en al veel in praktijk heb gebracht.
+        Na de Havo ben ik verder gaan zoeken naar een ICT-opleider die mij
+        aansprak. Dit heb ik gevonden bij Fontys Tilburg. Doordat hier op
+        projectbasis gewerkt wordt heb ik mij kunnen ontwikkelen in diverse
+        competenties. Na schooltijd werk ik af en toe voor Fontys videocrew. Ik
+        stel hier de projecten voor op (scripts) en presteer regelmatig de
+        video's. Wat ik erg leuk vind. Zo heb ik aandacht voor details,
+        transferabel code zal netjes zijn en voor andere te lezen. Mocht er iets
+        niet gaan zoals ik het wil kan ik door mijn aanpassingsvermogen en
+        oplossingsgerichtheid het op een andere manier proberen waardoor het wel
+        lukt. Door het doorzettingsvermogen ga ik ook daadwerkelijk door totdat
+        het er staat hoe ik of de klant het wil. Ik ben creatief, kan heel goed
+        zelfstandig werken, maar ook in teamverband zal ik mijn best doen om het
+        team op zijn best te kunnen laten functioneren. Analyseren wat een klant
+        graag wil en of dit haalbaar is, is het eerste waar ik mee begin.
+        Tussentijds en achteraf het presenteren van de ideeën en het werk dat je
+        gedaan. Wat ik in de lockdown de periode heb geleerd is discipline.
+        Tijdens de eerste 2 semesters hebben veel collega studenten hun weg naar
+        mij gevonden voor uitleg van de leerstof. Zij vonden de manier waarop
+        erg prettig maar didactische vaardigheden hebben we op school nog niet
+        gehad en ik hoop deze bij jullie te kunnen leren, om mijn specialisatie
+        ICT en educatie te kunnen afronden. Ik heb gemerkt dat ik kennis
+        overdragen erg leuk vindt en mij energie geeft. In de toekomst zou ik
+        graag als leraar op school willen werken, afgewisseld met een aantal
+        uren in mijn eigen web development bedrijf.
       </p>
     </div>
   );
